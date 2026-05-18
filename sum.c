537a2f4aa4b3fd3fce77f239f68c44fe1886a1e1
satoshi <bharatrokaya2007@gmail.com>
2026-05-18 17:37:19 +0545

    first

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Enter any two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
