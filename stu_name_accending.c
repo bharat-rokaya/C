83f0b2b76f9258c27074e327110c8a193e81b5ab
satoshi <bharatrokaya2007@gmail.com>
2026-05-18 17:57:38 +0545

    third

---
+++ stu_name_accending.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+    struct student {
+        char name[30];;
+        int roll;
+        float marks;
+    }, s[n], temp;
+
+    int n, i, j;
+    
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++) {
+        printf("Enter details for student %d:\n", i);
+        printf("Name: ");
+        scanf("%s", s[i].name);
+        printf("Roll: ");
+        scanf("%d", &s[i].roll);
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+    
+    for(i=1; i<=n; i++) {
+        for(j=i+1; j<=n; j++) {
+            if(strcmp(s[i].name, s[j].name) > 0) {
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+
+    printf("\n Sorted records are:");
+
+    for(i=1; i<=n; i++) {
+        printf("Name: %s \n", s[i].name);
+        printf("Roll: %d \n", s[i].roll);
+        printf("Marks: %.2f \n", s[i].marks);
+    }
+}    
